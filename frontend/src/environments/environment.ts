8e1c81249c8a299368a6e96cee49ee3d17dfa5e3
Mohammed Aziz Hammemi <Elhammemi001@gmail.com>
2026-08-31 09:29:25 +0100

    feat: add Firebase configuration to environment files

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8000'
+  apiBaseUrl: 'http://localhost:8000',
+  firebase: {
+    apiKey: "AIzaSyCNot3D6fJTIkFnRbZTIwdkh-9LTua-91o",
+    authDomain: "comptarag.firebaseapp.com",
+    projectId: "comptarag",
+    storageBucket: "comptarag.firebasestorage.app",
+    messagingSenderId: "87157798510",
+    appId: "1:87157798510:web:2d99236fde32d1a6af987f",
+    measurementId: "G-LWWVYX8XJ1"
+  }
 };
