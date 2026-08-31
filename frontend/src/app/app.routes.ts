c27215542ab79f31c85380c49cfd2c3db93caa17
Mohammed Aziz Hammemi <Elhammemi001@gmail.com>
2026-08-31 21:35:55 +0100

    fix: correct route definition for home component in app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    loadChildren: () => import('@features/home/home.routes').then((m) => m.HOME_ROUTES)
+  },
+  {
+    path: 'chat',
     loadChildren: () => import('@features/chat/chat.routes').then((m) => m.CHAT_ROUTES)
   },
   {
